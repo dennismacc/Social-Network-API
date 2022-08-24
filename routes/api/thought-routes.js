dcb2cc0ae6e67740e3ac02e9348b2ee2d74e8f84
dennismacc <dennis.maccarthy@gmail.com>
2022-08-23 17:43:54 -0700

    create thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,38 +3,75 @@ const router = require('express').Router();
 const { Thought, Reaction} = require('../../models')
 
 //TODO: ROUTE TO GET ALL THOUGHTS
-router.get('/', (req,res)=> {
-
-})
+router.get('/', async (req,res)=> {
+    try {
+        const thoughtsData = await Thought.findAll({include: [Reaction]})
+        res.json(thoughtsData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 //TODO: ROUTE TO CREATE A NEW THOUGHT
-router.post('/', (req,res)=> {
-
+router.post('/', async (req,res)=> {
+    try {
+        const newThought = await Thought.create(req.body)
+        res.status(201).json(newThought)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 });
 
 //TODO: ROUTE TO GET SINGLE THOUGHT BASED ON THOUGHT ID
-router.get('/:thoughtId', (req,res)=> {
-
-})
+router.get('/:thoughtId', async (req,res)=> {
+    try {
+        const thought = await Thought.findByPk(req.params.thoughtId, {include: [Reaction]})
+        res.json(thought)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 //TODO: ROUTE TO UPDATE A THOUGHT
-router.put('/', (req,res)=> {
-
-})
+router.put('/', async (req,res)=> {
+    try {
+        const updatedThought = await Thought.findOneAndUpdate({_id: req.body._id}, req.body, {new: true})
+        res.json(updatedThought)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 //TODO: ROUTE TO DELETE A THOUGHT BASED ON THOUGHT ID
-router.delete('/:thoughtId', (req,res)=> {
-
+router.delete('/:thoughtId', async (req,res)=> {
+    try {
+        const deletedThought = await Thought.findOneAndDelete({_id: req.params.thoughtId})
+        res.json(deletedThought)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 });
 
 //TODO: ROUTE TO ADD REACTION TO A THOUGHT
-router.post('/:thoughtId/reactions', (req,res)=> {
-
+router.post('/:thoughtId/reactions', async (req,res)=> {
+    try {
+        const newReaction = await Reaction.create(req.body)
+        const thought = await Thought.findOneAndUpdate({_id: req.params.thoughtId}, {$push: {reactions: newReaction._id}}, {new: true})
+        res.json(newReaction && thought)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 });
 
 //TODO: ROUTE TO DELETE A REACTION ON A THOUGHT
-router.delete('/:thoughtId/reactions/:reactionId', (req,res)=> {
-
-})
+router.delete('/:thoughtId/reactions/:reactionId', async (req,res)=> {
+    try {
+        const deletedReaction = await Reaction.findOneAndDelete({_id: req.params.reactionId})
+        const thought = await Thought.findOneAndUpdate({_id: req.params.thoughtId}, {$pull: {reactions: req.params.reactionId}}, {new: true})
+        res.json(deletedReaction && thought)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router;
